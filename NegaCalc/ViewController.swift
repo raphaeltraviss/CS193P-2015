16093a2f558d2b237b95deafe2b3c75fdd99f4ba
Raphael <raphael@skyleafdesign.com>
2015-07-26 09:36:38 -0500

    Initial Commit

---
+++ NegaCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NegaCalc
+//
+//  Created by Raphael on 7/26/15.
+//  Copyright (c) 2015 Skyleaf Design LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
